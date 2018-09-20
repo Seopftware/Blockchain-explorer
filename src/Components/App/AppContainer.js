5e2400abbdb4a399ac96f812700a97b78e96a984
Seopftware <inseop0813@gmail.com>
2018-09-20 15:27:46 +0900

    Navigation and Routes
    Making Navigation menu and routes

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -7,6 +7,9 @@ import typography from "../../typography";
 const baseStyles = () => injectGlobal `
     ${reset};
     ${typography};
+    a{
+        text-decoration:none!important;
+    }
 `;
 
 class AppContainer extends Component {
